4d3504000f73cd4734745f3c1e4ae6cab3d0af11
Colin Graydon <colingraydon@gmail.com>
2023-02-19 16:03:32 -0500

    updated gitignore

--- frontend/src/components/NewNotecard.tsx
+++ frontend/src/components/NewNotecard.tsx
@@ -1,17 +1,25 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
-import router from "next/router";
+import router, { useRouter } from "next/router";
 import React from "react";
+import { useCreateCardMutation, useMeQuery } from "../generated/graphql";
+import useIsAuth from "../utils/useIsAuth";
 import { withApollo } from "../utils/withApollo";
 import { InputField } from "./InputField";
 
 interface NewNotecardProps {}
 
 const NewNotecard: React.FC<NewNotecardProps> = ({}) => {
+  const { data, loading } = useMeQuery();
+  const router = useRouter();
+  useIsAuth();
+  const [createNotecards] = useCreateCardMutation();
+
   return (
     <Formik
       initialValues={{ title: "", text: "" }}
       onSubmit={async (values, { setErrors }) => {
+        const;
         // const {errors} = await createPost({
         //     variables: {input: values},
         //     //updateing apollo cache
